598295ab53d275cdc8eee41ac2f044e77810853b
Pedro Henrique Silva <pedroupandofacil@hotmail.com>
2023-09-24 19:13:40 -0300

    Arrumando a Indentação

--- components/php/post.php
+++ components/php/post.php
@@ -9,14 +9,14 @@
 <body>
 		<header class="header">
 			<nav class="header_nav">
-        <a href="../../index.html" class="nav_item">Inicio</a>
-        <a href="../pages/login.html" class="nav_item">Login</a>
-        <a href="../pages/cadastro.html" class="nav_item"> Cadastro </a>
-        <a href="../pages/menu.html" class="nav_item">Menu</a>
-        <a href="./tabela.php" class="nav_item">Tabela</a>
-        <a href="./data.php" class="nav_item">Data</a>
-        <a href="./get.php" class="nav_item">$_GET</a>
-        <a href="./post.php" class="nav_item">$_POST</a>
+        		<a href="../../index.html" class="nav_item">Inicio</a>
+        		<a href="../pages/login.html" class="nav_item">Login</a>
+        		<a href="../pages/cadastro.html" class="nav_item"> Cadastro </a>
+        		<a href="../pages/menu.html" class="nav_item">Menu</a>
+        		<a href="./tabela.php" class="nav_item">Tabela</a>
+        		<a href="./data.php" class="nav_item">Data</a>
+        		<a href="./get.php" class="nav_item">$_GET</a>
+        		<a href="./post.php" class="nav_item">$_POST</a>
 			</nav>
 		</header>
 
