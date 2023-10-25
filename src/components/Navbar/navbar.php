a3b9080cb934dbc07c5a19279a7cd42a8a43ddc1
Pedro Henrique Silva <pedroupandofacil@hotmail.com>
2023-10-25 18:10:53 -0300

    utilizando o session e o header

--- src/components/Navbar/navbar.php
+++ src/components/Navbar/navbar.php
@@ -1,10 +1,15 @@
+<?php
+  session_start();
+  $nomeUsuario = $_SESSION['usuario'];
+?>
+
 <header class="header">
   <div class="left_header_item">
     <p id="horaEdia"></p>
     <p id="diaDaSemana"></p>
   </div>
   <div class="right_header_item">
-    <p id="usuario">Nome de Usuário</p>
+    <p id="usuario"><?php echo $nomeUsuario; ?></p>
     <div class="frame"></div>
   </div>
 </header>
--- src/scripts/login.php
+++ src/scripts/login.php
@@ -1,15 +1,19 @@
 <?php
+
+    session_start();
     require("../php/conexao.php");
     if(isset($_POST['username']) && !empty($_POST['username']) || isset($_POST['password']) && !empty($_POST['password'])){
         $username = $_POST["username"];
         $password = $_POST["password"];
-        $sql = "select nm_login, cd_senha from tb_usuario where nm_login = '$username' and cd_senha = '$password'";
+        $sql = "select nm_usuariofrom tb_usuario where nm_login = '$username' and cd_senha = '$password'";
         $query = $pdo->prepare($sql);
         $query->execute();
         $user = $query->fetch();
         
         if($user){
-            echo "<script> window.location.href = '../php/dashboard.php' </script>";
+            $_SESSION['usuario'] = $user['nm_usuario'];
+            header('location:../php/dashboard.php');
+            // echo "<script> window.location.href = '../php/dashboard.php' </script>";
         }
         else{
             echo "<script> window.location.href = '../../login.html' </script>";
